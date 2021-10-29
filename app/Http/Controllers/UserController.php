2f7eb2d99221c5163b9e99549356af5fe7fe480a
mathieu-tulpinck <mathieu.tulpinck@student.ehb.be>
2021-10-29 12:17:52 +0000

    User as API resource. Creation of additional tenant migrations.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * This class was created with php artisan make:controller UserController --api
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        //TODO: implement validation logic
+
+        // Returns an array of the data that was validated.
+        $validatedAttributes = $request->validated();
+
+        return User::create($validatedAttributes);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $validatedAttributes = $request->validated();
+        $user->update($validatedAttributes);
+
+        return $user; //to be verified
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return User::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,8 @@ Route::middleware([
     Route::get('/', function () {
         return 'This is your central application.';
     });
+
+    Route::apiResource('users', UserController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
