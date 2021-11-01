ed3b9afd790f5897ff9bc7fdd62280b373f11aaa
mathieu-tulpinck <mathieu.tulpinck@student.ehb.be>
2021-11-01 11:05:13 +0000

    Add custom Domain model and modified tenancy config. Modified TenantResource to return DomainResource collection.

--- app/Http/Resources/TenantResource.php
+++ app/Http/Resources/TenantResource.php
@@ -14,6 +14,15 @@ class TenantResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request); // Default value.
+        return [
+            //dd($this),
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'data' => array('tenancy_db_name' => $this->tenancy_db_name),
+            'domains' => DomainResource::collection($this->domains)
+        ];
     }
 }
