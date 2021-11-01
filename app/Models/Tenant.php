85a78fa3982c1db6dbaedc57bf22343abfd93b7e
mathieu-tulpinck <mathieu.tulpinck@student.ehb.be>
2021-11-01 09:46:05 +0000

    Set-up of tenant migrations and db seeds.

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
@@ -9,7 +10,7 @@ use Stancl\Tenancy\Database\Concerns\HasDomains;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    use HasDatabase, HasDomains;
+    use HasDatabase, HasDomains, HasFactory;
 
     protected $fillable = ['name'];
 
--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -2,20 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TenantDatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
         User::factory(1)->create();
-        Tenant::factory()->create()->domains()->create(['domain' => 'demo.hexclan.test']);
     }
 }
