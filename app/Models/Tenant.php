5be7f4d9cf57e53ef599341a80622458c46e1381
olivierthas <olivier.thas@gmail.com>
2021-11-24 17:57:01 +0100

    Welcome, Tenant, composer.lock

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -15,6 +15,11 @@ class Tenant extends BaseTenant implements TenantWithDatabase
     // Domains relationship are always.
     protected $with = ['domains'];
 
+    // Protection against "mass assignment vulnerability" 
+    // The fillable property specifies which attributes can be mass assigned. 
+    // In other words, these attributes or fields are the only ones that can be mass assigned or uploaded. All others will be ignored.
+    // This is for default security. Imagine if a user mass assigns a tenant or updates the admin email.
+    // when setting this fillable property, we say: "even if other attributes are included here, the only ones allowed to be fillable are 'name' and 'tenancy_admin_email' ".
     protected $fillable = ['name', 'tenancy_admin_email'];
 
     public static function getCustomColumns(): array
