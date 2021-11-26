cb337b00456a49135ea31b5f623c2cc861012814
olivierthas <olivier.thas@gmail.com>
2021-11-26 15:47:16 +0000

    Bug fixing prior to demo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
